ad313d78b3f19ebe5f0767f4c0b98319eb4a0647
ZQCinderella <604017846@qq.com>
2016-11-12 11:00:17 +0800

    '最世界项目'

---
+++ webstorm/www/js/app/citywalk.js
@@ -0,0 +1,167 @@
+/**
+ * Created by Administrator on 2016/11/10.
+ */
+define(['jquery'],function($){
+    function getCityData(){
+        $.ajax({
+            url:'http://localhost:8000/citywalk',
+            type:'get',
+            success:handleData
+        });
+    }
+    function handleData(data){
+        //根据数据长度创建节点
+        $(data).each(function(index,elem){
+            var divProduct = $('<div class="product"></div>');
+            var img = $('<img/>');
+            img.attr('src',elem['imgurl']);
+            divProduct.append(img);
+
+            //详细信息
+            var proInfo = $('<div class="pro-info"></div>');
+            var infoPlace = $('<span class="info-place"></span>');
+            infoPlace.html(elem['address']);
+            proInfo.append(infoPlace);
+
+            //浏览量和已售件数
+            var infoScan = $('<div class="info-scan"></div>');
+            var browseCount = $('<span></span>');
+            browseCount.html(elem['browseCount']);
+            var soldCount = $('<span></span>');
+            soldCount.html(elem['soldCount']);
+            infoScan.append(browseCount);
+            infoScan.html(infoScan.html() +'次浏览');
+
+            // var text1 = document.createTextNode('次浏览');
+            // var text2 = document.createTextNode('件已售');
+            // infoScan[0].appendChild(text1);
+            // infoScan[0].appendChild(text2);
+            infoScan.append(soldCount);
+            infoScan.html(infoScan.html() +'件已售' );
+            proInfo.append(infoScan);
+
+            //h2标题
+            var h2 = $('<h2></h2>');
+            var a = $('<a></a>');
+            a.html(elem['title']);
+            h2.append(a);
+            proInfo.append(h2);
+
+            //ul列表
+            var ul = $('<ul></ul>');
+            //遍历列表长度，并创建li
+            $(elem['introduce']).each(function(index,elem){
+                var li = $('<li></li>');
+                li.html(elem);
+                ul.append(li);
+            });
+            proInfo.append(ul);
+
+            //商品价格
+            var infoPrice = $('<div class="info-price"></div>');
+            var oldPrice = $('<s></s>');
+            oldPrice.html(elem['oldPrice'] + '元');
+            var newPrice = $('<strong></strong>');
+            newPrice.html(elem['newPrice']);
+            infoPrice.append(oldPrice);
+            infoPrice.append(newPrice);
+            infoPrice.html(infoPrice.html() + '元起');
+            proInfo.append(infoPrice);
+
+            //提交按钮
+            var submit = $('<div class="submit"></div>');
+            var submitAa = $('<a href="#"></a>');
+            submitAa.html('立即预订');
+            submit.append(submitAa);
+            proInfo.append(submit);
+
+            divProduct.append(proInfo);
+            //拼接到html中
+            $('.ulDiv').append(divProduct);
+        });
+        //控制分页
+        var pageSize = 1;
+        //动态创建页数
+        for(var i = 0 ; i < $('.ulDiv').children().length /pageSize ; i++){
+            var a = $('<a href="#"></a>');
+            a.html(i+1);
+            $('.pageNum').append(a);
+        }
+        //控制每页显示的product数量
+        $('.product:gt('+(pageSize - 1)+ ')').hide();
+        //第一页默认被选中
+        $('.pageNum a').eq(0).addClass('active');
+
+
+        //设置超链接点击换页事件
+        $('.pageNum a').on('click',function(){
+            $(this).attr('class','active').siblings().removeClass('active');
+            var index = parseInt($(this).prev().html());
+
+            var startIndex = (index)*pageSize;
+            showPageContent(startIndex,index,pageSize);
+            showPage();
+
+            return false;
+        });
+        //下一页事件
+        $('.nextPage').on('click',function(){
+             //获取当前页
+            var index = parseInt($('.pageNum a.active').html());
+
+            //讲当前页的class = active移除，讲下一页的添加class
+            $('.pageNum a').eq(index -1).removeClass('active').next().addClass('active');
+
+            var startIndex = (index)*pageSize;
+            //重复代码
+            showPageContent(startIndex,index,pageSize);
+
+            showPage();
+            return false;
+        });
+        //上一页事件
+        $('.prePage').on('click',function () {
+            //获取将要加载的页
+            var index = parseInt($('.pageNum a.active').prev().html());
+            //讲当前页的class = active移除，讲下一页的添加class
+            $('.pageNum a').eq(index).removeClass('active').prev().addClass('active');
+
+            var startIndex = (index - 1)*pageSize;
+
+            showPageContent(startIndex,index,pageSize);
+
+            showPage();
+
+            return false;
+        });
+    }
+
+    //显示当前页的内容
+    function showPageContent(startIndex,index,pageSize){
+        if(startIndex == 0 || isNaN(index)){
+            $('.product:lt('+( pageSize )+ ')').show();
+            $('.product:gt('+(pageSize - 1)+ ')').hide();
+        }else{
+
+            $('.product').hide();
+            $('.product:eq('+(startIndex - 1)+ ')').nextUntil( $('.product:eq('+(startIndex + pageSize)+ ')')).show();
+        }
+    }
+    //显示上一页和下一页
+    function showPage(){
+        //如果不再是第一页，则显示上一页
+        if(!$('.pageNum a').eq(0).hasClass('active')){
+            $('.prePage').show();
+        }else{
+            $('.prePage').hide();
+        }
+        //如果是最后一页，则让下一页隐藏
+        if($('.pageNum a').last().hasClass('active')){
+            $('.nextPage').hide();
+        }else{
+            $('.nextPage').show();
+        }
+
+    }
+    return getCityData;
+});
---
+++ webstorm/www/js/app/freeWalk.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Administrator on 2016/11/9.
+ */
+define(['jquery'],function($){
+    hoverLi();
+    function getFWData(){
+        $.ajax({
+            type:"get",
+            url:"http://localhost:8000/fw",
+            success:handleFw
+        })
+    }
+    return getFWData;
+})
+function handleFw(data){
+    $(data).each(function(index,elem) {
+        $(".fwTitle li a").eq(index).text(elem.title);
+        var datas = elem.data;
+
+        $('.fwContet').eq(index).find('li').each(function(ind,ele){
+            if(ind < 6){
+                //设置图片
+                $(this).find('img').attr('src',datas[ind]['imgUrl']);
+                //设置标题和价格
+                $(this).find('.price').find('em').text(datas[ind]['price']);
+                $(this).find('h3').text(datas[ind]['title']);
+            }
+        });
+    });
+}
+function hoverLi(){
+    $(".fwTitle li").on("mouseenter",function(){
+        $(".ziyouxing-content ul").eq($(this).index()).show().siblings().hide();
+    })
+}
